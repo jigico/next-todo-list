d60a9aa1e0483b683dfd3bb05bd540368556afaa
jisu <ahteostnqls@naver.com>
2024-03-12 17:13:53 +0900

    [MOD] home 경로 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ export const Nav = () => {
   return (
     <header className="p-8 shadow">
       <nav className="flex items-center justify-center gap-4">
-        <Link href="">home</Link>
+        <Link href="/">home</Link>
         <Link href="/about">about</Link>
         <Link href="/report">report</Link>
         <Link href="/todosCsr">todos-csr</Link>
