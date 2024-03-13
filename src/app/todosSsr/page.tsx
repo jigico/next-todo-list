5da1e89c014ec4b141d25b4261716a3373a69d57
jisu <ahteostnqls@naver.com>
2024-03-13 21:12:53 +0900

    [ADD] todo ssr page 데이터 조회 작업

--- src/app/todosSsr/page.tsx
+++ src/app/todosSsr/page.tsx
@@ -1,7 +1,25 @@
+import { Todos } from "@/types/todos";
 import React from "react";
 
-const todosSsrPage = () => {
-  return <div>todosSsrPage</div>;
+const todosSsrPage = async () => {
+  const response = await fetch("http://localhost:3000/api/todos");
+  const todos = await response.json();
+
+  console.log(todos);
+
+  return (
+    <ul>
+      {todos?.map((todo: Todos) => {
+        return (
+          <li key={todo.id}>
+            {todo.title}
+            <p>{todo.contents}</p>
+            <p>{todo.isDone ? "처리완료" : "처리중"}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default todosSsrPage;
