1dccfcadcbc03d3f34d1469a91eb277b342e2d66
jisu <ahteostnqls@naver.com>
2024-03-15 00:11:56 +0900

    [ADD] route handler patch - 완료 여부 변경 작업

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -6,3 +6,23 @@ export async function DELETE(_request: Request, { params }: { params: { id: stri
   const data = await response.json();
   return Response.json({ data });
 }
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+  const { isDone } = await request.json();
+  const { id } = params;
+  const response = await fetch(`http://localhost:4000/todos/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ isDone })
+  });
+
+  const updateTodo = await response.json();
+  return new Response(JSON.stringify(updateTodo), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
