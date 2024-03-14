ebb908463832ba3a119081923a9011c1037a2aa3
jisu <ahteostnqls@naver.com>
2024-03-14 19:23:49 +0900

    [ADD] todo 삭제 기능 작업

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,40 @@
 "use client";
 
 import type { Todos } from "@/types/todos";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 
 export const TodoList = () => {
   const { data, isLoading, isError } = useQuery<Todos[]>({
     queryFn: async () => {
-      const response = await fetch("http://localhost:3000/api/todos");
+      const response = await fetch("/api/todos");
       const todos = response.json();
       return todos;
     },
     queryKey: ["todos"]
   });
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation({
+    mutationFn: async (id: string) => {
+      const response = await fetch(`/api/todos/${id}`, {
+        method: "DELETE"
+      });
+      await response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    }
+  });
+
+  //todo 삭제
+  const handleDeleteTodo = (id: string) => {
+    if (window.confirm("정말 삭제할까요?")) {
+      deleteMutation.mutate(id);
+      alert("삭제를 완료했습니다.");
+    } else {
+      alert("삭제를 취소했습니다.");
+    }
+  };
 
   if (isError) {
     return <div>에러가 발생했습니다</div>;
@@ -31,7 +53,7 @@ export const TodoList = () => {
             <p>{todo.contents}</p>
             <div>
               <button>{todo.isDone ? "취소" : "완료"}</button>
-              <button>삭제</button>
+              <button onClick={() => handleDeleteTodo(todo.id)}>삭제</button>
             </div>
           </li>
         );
