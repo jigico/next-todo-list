34b8858990523059ee5bfc0f7c2070e6d9e54ab4
jisu <ahteostnqls@naver.com>
2024-03-14 17:35:36 +0900

    [MOD] useQuery 삭제 및 fetch로 변경

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,33 +1,15 @@
-"use client";
-
 import type { CompanyInfo } from "@/types/CompanyInfo";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
-const AboutPage = () => {
-  const { data, isLoading, isError } = useQuery<CompanyInfo>({
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3000/api/company");
-      const company = await response.json();
-
-      return company;
-    },
-    queryKey: ["companyInfo"]
-  });
-
-  if (isError) {
-    return <div>에러가 발생했습니다</div>;
-  }
-
-  if (isLoading) {
-    return <div>로딩중</div>;
-  }
+const AboutPage = async () => {
+  const response = await fetch("http://localhost:4000/companyInfo");
+  const companyInfo: CompanyInfo = await response.json();
 
   return (
     <ul>
-      <li>회사명: {data?.name}</li>
-      <li>회사 소개: {data?.description}</li>
-      <li>회사 홈페이지 주소: {data?.url}</li>
+      <li>회사명: {companyInfo?.name}</li>
+      <li>회사 소개: {companyInfo?.description}</li>
+      <li>회사 홈페이지 주소: {companyInfo?.url}</li>
     </ul>
   );
 };
