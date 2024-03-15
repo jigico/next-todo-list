cff9792e43c491f70f4d11af0feba61e2f197c41
jisu <ahteostnqls@naver.com>
2024-03-15 12:50:49 +0900

    [ADD] 메인화면 디자인 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">메인</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
+      안녕하세요
+      <br />
+      로컬 컴퍼니에 오신걸 환영합니다. <br />
+      로컬 컴퍼니의 할일을 볼 수 있습니다.
+    </main>
+  );
 }
