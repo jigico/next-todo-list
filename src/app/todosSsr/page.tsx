99bef7009d8efe6e488135740f18bb2daf54b7d8
jisu <ahteostnqls@naver.com>
2024-03-15 12:28:11 +0900

    [MOD] UI 스타일링 작업

--- src/app/todosSsr/page.tsx
+++ src/app/todosSsr/page.tsx
@@ -2,7 +2,7 @@ import { Todos } from "@/types/todos";
 import Link from "next/link";
 import React from "react";
 
-const todosSsrPage = async () => {
+const TodosSsrPage = async () => {
   const response = await fetch("http://localhost:4000/todos", {
     cache: "no-cache"
   });
@@ -10,14 +10,16 @@ const todosSsrPage = async () => {
 
   return (
     <>
-      <Link href="/report">할일정보통계 보러가기</Link>
-      <ul>
+      <Link href="/report" className="btn btn-xs">
+        할일정보통계 보러가기
+      </Link>
+      <ul className="mt-4 mb-8 grid grid-cols-4 gap-4">
         {todos?.map((todo: Todos) => {
           return (
-            <li key={todo.id}>
-              {todo.title}
+            <li key={todo.id} className="shadow p-4 rounded">
+              {todo.isDone ? <p className="badge badge-primary badge-outline">처리완료</p> : <p className="badge badge-secondary badge-outline">처리중</p>}
+              <p className="font-bold">{todo.title}</p>
               <p>{todo.contents}</p>
-              <p>{todo.isDone ? "처리완료" : "처리중"}</p>
             </li>
           );
         })}
@@ -26,4 +28,4 @@ const todosSsrPage = async () => {
   );
 };
 
-export default todosSsrPage;
+export default TodosSsrPage;
