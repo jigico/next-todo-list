53d955a357ee46b832c24aa90b78882e1345a0e9
jisu <ahteostnqls@naver.com>
2024-03-15 17:58:56 +0900

    [ADD] try-catch문 추가

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,28 +1,44 @@
 export async function DELETE(_request: Request, { params }: { params: { id: string } }) {
-  const { id } = params;
-  const response = await fetch(`http://localhost:4000/todos/${id}`, {
-    method: "DELETE"
-  });
-  const data = await response.json();
-  return Response.json({ data });
+  try {
+    const { id } = params;
+    const response = await fetch(`http://localhost:4000/todos/${id}`, {
+      method: "DELETE"
+    });
+    const data = await response.json();
+    return Response.json({ data });
+  } catch (error) {
+    // 에러 처리
+    console.error("An error occurred while fetching todos:", error);
+    return new Response("An error occurred while fetching todos", {
+      status: 500
+    });
+  }
 }
 
 export async function PATCH(request: Request, { params }: { params: { id: string } }) {
-  const { isDone } = await request.json();
-  const { id } = params;
-  const response = await fetch(`http://localhost:4000/todos/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ isDone })
-  });
+  try {
+    const { isDone } = await request.json();
+    const { id } = params;
+    const response = await fetch(`http://localhost:4000/todos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ isDone })
+    });
 
-  const updateTodo = await response.json();
-  return new Response(JSON.stringify(updateTodo), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
+    const updateTodo = await response.json();
+    return new Response(JSON.stringify(updateTodo), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  } catch (error) {
+    // 에러 처리
+    console.error("An error occurred while fetching todos:", error);
+    return new Response("An error occurred while fetching todos", {
+      status: 500
+    });
+  }
 }
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,39 +1,55 @@
 export async function GET(request: Request) {
-  const response = await fetch("http://localhost:4000/todos");
-  const todos = await response.json();
+  try {
+    const response = await fetch("http://localhost:4000/todos");
+    const todos = await response.json();
 
-  if (!todos) {
-    return new Response("todos is not found", {
-      status: 404
+    if (!todos) {
+      return new Response("todos is not found", {
+        status: 404
+      });
+    }
+
+    return Response.json(todos);
+  } catch (error) {
+    // 에러 처리
+    console.error("An error occurred while fetching todos:", error);
+    return new Response("An error occurred while fetching todos", {
+      status: 500
     });
   }
-
-  return Response.json(todos);
 }
 
 export async function POST(request: Request) {
-  const { title, contents } = await request.json();
+  try {
+    const { title, contents } = await request.json();
 
-  if (!title) {
-    return new Response("title is not found", {
-      status: 404
+    if (!title) {
+      return new Response("title is not found", {
+        status: 404
+      });
+    }
+
+    if (!contents) {
+      return new Response("contents is not found", {
+        status: 404
+      });
+    }
+
+    const response = await fetch("http://localhost:4000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title, contents, isDone: false })
+    });
+
+    const todo = await response.json();
+    return Response.json(todo);
+  } catch (error) {
+    // 에러 처리
+    console.error("An error occurred while fetching todos:", error);
+    return new Response("An error occurred while fetching todos", {
+      status: 500
     });
   }
-
-  if (!contents) {
-    return new Response("contents is not found", {
-      status: 404
-    });
-  }
-
-  const response = await fetch("http://localhost:4000/todos", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ title, contents, isDone: false })
-  });
-
-  const todo = await response.json();
-  return Response.json(todo);
 }
