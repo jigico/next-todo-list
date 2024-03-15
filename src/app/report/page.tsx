f0060fd8f081e0e3f9e6c61e6921163c012b1525
jisu <ahteostnqls@naver.com>
2024-03-15 11:12:21 +0900

    [ADD] todo 완료. 미완료 수 조회 기능 작업

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,7 +1,18 @@
+import type { Todos } from "@/types/todos";
 import React from "react";
 
-const reportPage = () => {
-  return <div>reportPage</div>;
+const ReportPage = async () => {
+  const response = await fetch("http://localhost:4000/todos");
+  const todos: Todos[] = await response.json();
+
+  return (
+    <div>
+      <p>현재까지 {todos.length}개의 todolist가 등록되었습니다.</p>
+      <p>
+        할일 수 : {todos.filter((todo) => todo.isDone === false).length}개 / 완료 수 : {todos.filter((todo) => todo.isDone === true).length}개
+      </p>
+    </div>
+  );
 };
 
-export default reportPage;
+export default ReportPage;
